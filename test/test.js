4a412c69336d548826619e6f7e62e60b71a3ce59
ajb333111 <birdaj@miamioh.edu>
2017-12-06 14:28:05 -0500

    Added 3rd test testing response status

--- test/test.js
+++ test/test.js
@@ -2,7 +2,11 @@ var expect = require("chai").expect;
 var assert = require('assert');
 var base_url = "http://localhost:3000/";
 var userTest = require("../userTest.js");
+var routes = require("../routes");
 var user = {name: "Test"};
+var should = require('chai').should();
+var supertest = require('supertest');
+var api = supertest('http://localhost:3000');
 
 describe('Database', function(){
 	it('Should show that database is functional', function(){
@@ -13,6 +17,14 @@ describe('Database', function(){
 describe('Homepage', function(){
 	it('should have name National Coding League', function(){
 		expect(user).to.have.property('name');
+	});
+});
+
+describe('Registration Page', function(){
+	it('should return a 200 reponse', function(done){
+		api.get('/routes/register.js')
+		.set('Accept', 'app/package.json')
+		.expect(200, done);
 	});
 });
 
